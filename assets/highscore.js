3c9419e6eb4248e36e10586662485c7365cd2acb
IFSCM <119161184+IFSCM@users.noreply.github.com>
2024-05-22 23:17:51 +0530

    Update highscore.js

--- assets/highscore.js
+++ assets/highscore.js
@@ -2,8 +2,9 @@
 const gameVersion = "4.8";
 const relay = "https://varied-peggi-coredigital-47cb7fd7.koyeb.app/relay?link=";
 const scoreEndpoint = "http://ec2-3-8-192-132.eu-west-2.compute.amazonaws.com:4040";
-const relayedEndpoint = relay + scoreEndpoint;
 const restrictAll = false;
+const relayedEndpoint = relay + scoreEndpoint;
+
 
 async function populateHS() {
     // Fetch data from the endpoint
